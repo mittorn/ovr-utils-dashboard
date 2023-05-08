0c2391c4e9a0c947b3f5d7125d0f883cc4f05402
mittorn <mittorn@sibmail.com>
2023-05-08 04:10:31 +0300

    enable global input by default

--- src/addons/openvr_overlay/overlay_init.gd
+++ src/addons/openvr_overlay/overlay_init.gd
@@ -8,6 +8,8 @@ const OVERLAY_PROPERTIES_DEFAULT = {
 	"has_cursor": false,
 	"has_touch": false,
 	"has_grab": true,
+	"width": 1024,
+	"height": 1024,
 }
 
 var ovr_interface: ARVRInterface
@@ -22,6 +24,9 @@ var trackers = {
 
 func _init() -> void:
 #	OS.window_minimized = true
+	if OS.has_feature("editor"):
+		OS.execute('killall',['ovr-utils.x86_64'])
+	ovr_config.set_global_overlay_input(true)
 	ovr_config.set_application_type(2) # Set to OVERLAY MODE
 	ovr_config.set_tracking_universe(1) # Set to SEATED MODE = 0, STANDING MODE = 1, RAW MODE = 2
 
@@ -29,6 +34,13 @@ func _init() -> void:
 	ovr_interface = ARVRServer.find_interface("OpenVR")
 	if ovr_interface and ovr_interface.initialize():
 		print("OpenVR Initialized.")
+		# make sure vsync is disabled or we'll be limited to 60fps
+	OS.vsync_enabled = false
+
+	# up our physics to 90fps to get in sync with our rendering
+	Engine.iterations_per_second = 90
+	Engine.target_fps = 90
+
 
 
 func _ready() -> void:
