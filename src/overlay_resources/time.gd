51b0a05919553d5e17e22f6bd66b5ba23c367e76
mittorn <mittorn@sibmail.com>
2023-05-08 04:24:20 +0300

    Small edits

--- src/overlay_resources/time.gd
+++ src/overlay_resources/time.gd
@@ -17,6 +17,8 @@ func _process(delta: float) -> void:
 func update_time():
 	var h = str(OS.get_time().hour)
 	var m = str(OS.get_time().minute)
+	var s = str(OS.get_time().second)
 	h = h if len(h) == 2 else "0" + h
 	m = m if len(m) == 2 else "0" + m
-	text = h + ":" + m
+	s = s if len(s) == 2 else "0" + s
+	text = h + ":" + m + ':' + s
