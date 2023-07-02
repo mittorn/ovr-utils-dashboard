5a02e5516517e3627fb7a0dde469676a74a1d574
mittorn <mittorn@sibmail.com>
2023-07-03 01:28:18 +0300

    Reduce code duplication

--- src/overlay_resources/compwindow.gd
+++ src/overlay_resources/compwindow.gd
@@ -18,18 +18,7 @@ var OVERLAY_PROPERTIES = {
 var instance
 func pre_prop_callback(_instance):
 	instance = _instance
-	if instance.compwindow_class < 0 || instance.compwindow_class > 2:
-		instance.compwindow_class = 0;
-	$Buttons/Index.text = str(instance.compwindow_index)
-	$Buttons/Classname.text = classnames[instance.compwindow_class]
-	var window = compwindow.register_window(classnames[instance.compwindow_class],instance.compwindow_index)
-	print(window)
-	$Buttons/Title.text = compwindow.get_window_title()
-	if window:
-		OVERLAY_PROPERTIES.width = max(compwindow.get_width(), 320)
-		OVERLAY_PROPERTIES.height = max(compwindow.get_height()+40, 40)
-
-
+	update_window(true)
 
 var t = 0
 var tex
@@ -55,8 +44,7 @@ func _draw() -> void:
 	if compwindow.get_id() && !tex:
 		update_texture()
 	
-
-func update_window():
+func update_window(first = false):
 	if instance.compwindow_class < 0 || instance.compwindow_class > 2:
 		instance.compwindow_class = 0;
 	$Buttons/Index.text = str(instance.compwindow_index)
@@ -67,10 +55,12 @@ func update_window():
 	if window:
 		OVERLAY_PROPERTIES.width  = max(compwindow.get_width(), 320)
 		OVERLAY_PROPERTIES.height = max(compwindow.get_height()+40, 40)
-		instance.OVERLAY_PROPERTIES.width = OVERLAY_PROPERTIES.width
-		instance.OVERLAY_PROPERTIES.height = OVERLAY_PROPERTIES.height
-		instance.try_update_size()
-	update_texture()
+		if !first:
+			instance.OVERLAY_PROPERTIES.width = OVERLAY_PROPERTIES.width
+			instance.OVERLAY_PROPERTIES.height = OVERLAY_PROPERTIES.height
+			instance.try_update_size()
+	if !first:
+		update_texture()
 	pass
 
 func _on_ButtonPrev_pressed():
