245e7f1e598096aa424c6d5e001a0c833e924f16
mittorn <mittorn@sibmail.com>
2023-07-12 12:48:47 +0300

    Change popup raise_flags to make browser popups work better

--- src/overlay_resources/Popup.gd
+++ src/overlay_resources/Popup.gd
@@ -9,7 +9,7 @@ func find_window():
 	var window = 0
 	if ScreenGrab.popup_target:
 		window = compwindow.register_popup(ScreenGrab.popup_target, instance.compwindow_index)
-		raise_flags = 0
+		raise_flags = 3
 		print('popup', window)
 	if instance.compwindow_index == 0:
 		ScreenGrab.popups_visible = (window != 0)
